01d471608199d9654f287ed491d8a900635fe545
hamzatoor251 <hamzamushtaqtoor@gmail.com>
2023-05-08 00:29:08 +0500

    task 2 added

---
+++ Gridview-Task-2/List_Page.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="List_Page.aspx.cs" Inherits="Gridview_Assign_2.List_Page" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"/>
+    <script>
+        function ValidSearch() {
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2 class="auto-style1">List Page of Employees</h2>
+            <h5 >Search Memebr: </h5>
+            <asp:TextBox runat="server" id="search" placeholder="Search member name...."></asp:TextBox>
+            <asp:Button runat="server" ID="callSearch" Text="Search" OnClick="callSearch_Click"/>
+            <asp:Button runat="server" ID="backCall" Text="Back" OnClick="backCall_Click"/>
+
+            <asp:Label runat="server" ID="result"></asp:Label>
+            <asp:gridview  id="Satffgrid" runat="server" backcolor="White" bordercolor="#E7E7FF"
+
+            borderstyle="None" borderwidth="1px"  AlternatingRowStyle-BorderWidth="2px" cellpadding="3" font-names="Calibri" font-size="Larger"
+
+            gridlines="Horizontal"  AllowPaging="true" OnPageIndexChanging="GridView_PageIndexChanging" PageSize="15" DataKeyNames="ID" OnRowEditing="Satffgrid_RowEditing" OnRowCancelingEdit="Satffgrid_RowCancelingEdit" OnRowUpdating="Satffgrid_RowUpdating" OnRowDeleting="Satffgrid_RowDeleting" OnRowDataBound="Satffgrid_RowDataBound">
+            
+            <AlternatingRowStyle BackColor="#F7F7F7" />
+
+            <FooterStyle BackColor="#B5C7EE" ForeColor="#4A3C88" />
+
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+
+            <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+
+            <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+
+            <SortedAscendingCellStyle BackColor="#F4F4FD" />
+
+            <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+
+            <SortedDescendingCellStyle BackColor="#D8D8F0" />
+
+            <SortedDescendingHeaderStyle BackColor="#3E3277" />
+            <Columns>
+                <asp:CommandField ShowEditButton="true" />  
+                <asp:CommandField ShowDeleteButton="true" />
+                
+            </Columns>
+
+        </asp:gridview>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ Gridview-Task-2/List_Page.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gridview_Assign_2
+{
+    public partial class List_Page : System.Web.UI.Page
+    {
+        public List<StaffMember> newStaff { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!Page.IsPostBack && Session["Member"]==null)
+            {
+                this.sessionGrid();
+                this.BindGrid();
+            }
+            else
+            {
+                this.BindGrid();
+            }
+
+        }
+        public class StaffMember
+        {
+            public int Rndmnum()
+            {
+                Random rnd = new Random();
+                int rndID = rnd.Next(00, 99);
+                return rndID;
+            }
+            //public List<StaffMember> newStaff = new List<StaffMember>();
+            public string Name { get; set; }
+            public string ID { get; set; }
+            public string Address { get; set; }
+            public List<StaffMember> MakeEmp()
+            {
+                List<StaffMember> newStaff = new List<StaffMember>();
+                newStaff.Add(new StaffMember() { Name = "Ali", ID = $"01{Rndmnum()}", Address = "Founder" });
+                newStaff.Add(new StaffMember() { Name = "Ahmad", ID = $"02{Rndmnum()}", Address = "CEO" });
+                newStaff.Add(new StaffMember() { Name = "Ghufi", ID = $"03{Rndmnum()}", Address = "BOD" });
+                newStaff.Add(new StaffMember() { Name = "Ibi", ID = $"04{Rndmnum()}", Address = "BOD" });
+                newStaff.Add(new StaffMember() { Name = "Haday", ID = $"05{Rndmnum()}", Address = "Staff Manager" });
+                newStaff.Add(new StaffMember() { Name = "Iram", ID = $"06{Rndmnum()}", Address = "Branch Manager" });
+                newStaff.Add(new StaffMember() { Name = "Rida", ID = $"07{Rndmnum()}", Address = "Branch Manager" });
+                newStaff.Add(new StaffMember() { Name = "Tahir", ID = $"08{Rndmnum()}", Address = "HR Manager" });
+                newStaff.Add(new StaffMember() { Name = "Bilal", ID = $"09{Rndmnum()}", Address = "HR Manager" });
+                newStaff.Add(new StaffMember() { Name = "Moadib", ID = $"10{Rndmnum()}", Address = "Associate SA" });
+                newStaff.Add(new StaffMember() { Name = "Nasir", ID = $"11{Rndmnum()}", Address = "Associate SA" });
+                newStaff.Add(new StaffMember() { Name = "Osama", ID = $"12{Rndmnum()}", Address = "Associate SA" });
+                newStaff.Add(new StaffMember() { Name = "Roshan", ID = $"13{Rndmnum()}", Address = "Senior .Net Dev" });
+                newStaff.Add(new StaffMember() { Name = "Awais", ID = $"14{Rndmnum()}", Address = "Senior .Net Dev" });
+                newStaff.Add(new StaffMember() { Name = "Kazim", ID = $"15{Rndmnum()}", Address = "Senior .Net Dev" });
+                newStaff.Add(new StaffMember() { Name = "Numan", ID = $"16{Rndmnum()}", Address = "Senior Mern dev" });
+                newStaff.Add(new StaffMember() { Name = "Raza", ID = $"17{Rndmnum()}", Address = "Senior Mern dev" });
+                newStaff.Add(new StaffMember() { Name = "Omer", ID = $"18{Rndmnum()}", Address = "Senior Mern dev" });
+                newStaff.Add(new StaffMember() { Name = "Ashraf", ID = $"19{Rndmnum()}", Address = "Bussiner Development" });
+                newStaff.Add(new StaffMember() { Name = "Asaad", ID = $"20{Rndmnum()}", Address = "Bussiner Development" });
+                newStaff.Add(new StaffMember() { Name = "Majid", ID = $"21{Rndmnum()}", Address = "Bussiner Development" });
+                newStaff.Add(new StaffMember() { Name = "Awais", ID = $"22{Rndmnum()}", Address = "Senior Mean dev" });
+                newStaff.Add(new StaffMember() { Name = "Hadir", ID = $"23{Rndmnum()}", Address = "Senior Mean dev" });
+                newStaff.Add(new StaffMember() { Name = "Atif", ID = $"24{Rndmnum()}", Address = "Full stack dev" });
+                newStaff.Add(new StaffMember() { Name = "Abdullah", ID = $"25{Rndmnum()}", Address = "Full stack dev" });
+                newStaff.Add(new StaffMember() { Name = "Shaukat", ID = $"26{Rndmnum()}", Address = "Full stack dev" });
+                newStaff.Add(new StaffMember() { Name = "Butt", ID = $"27{Rndmnum()}", Address = "Full stack dev" });
+                newStaff.Add(new StaffMember() { Name = "Burhan", ID = $"28{Rndmnum()}", Address = "Full stack dev" });
+                newStaff.Add(new StaffMember() { Name = "Azeem", ID = $"29{Rndmnum()}", Address = "Full stack dev" });
+                newStaff.Add(new StaffMember() { Name = "Sikander", ID = $"30{Rndmnum()}", Address = "Full stack dev" });
+
+                return newStaff;
+            }
+
+
+        }
+        public void sessionGrid()
+        {
+            List<StaffMember> makeList = new StaffMember().MakeEmp();
+            Session["Member"] = makeList;
+            List<StaffMember> retList = Session["Member"] as List<StaffMember>;
+            //return retList;
+
+        }
+        public void BindGrid()
+        {
+            Satffgrid.DataSource = Session["Member"] as List<StaffMember>;
+            Satffgrid.DataBind();
+        }
+        protected void GridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            Satffgrid.PageIndex = e.NewPageIndex;
+            BindGrid();
+        }
+
+        public void callSearch_Click(object sender, EventArgs e)
+        {
+            List<StaffMember> filter = new List<StaffMember>();
+            foreach (var i in Session["Member"] as List<StaffMember>)
+            {
+                if ((i.Name.ToLower() == search.Text.ToLower()) || i.ID.ToLower()==search.Text.ToLower() || i.Address.ToLower()==search.Text.ToLower())
+                {
+                    filter.Add(i);
+                }
+            }
+            Satffgrid.DataSource = filter;
+            Satffgrid.DataBind();
+        }
+
+        protected void Satffgrid_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            Satffgrid.EditIndex = e.NewEditIndex;
+            BindGrid();
+        }
+
+        protected void Satffgrid_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            Satffgrid.EditIndex = -1;
+            BindGrid();
+        }
+
+        protected void Satffgrid_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            List<StaffMember> updateGrid = new List<StaffMember>();
+            //updateGrid.Add(e.RowIndex);
+            //Session["Member"] = updateGrid;
+            //BindGrid();
+            TextBox name = (TextBox)Satffgrid.Rows[e.RowIndex].FindControl("Name") as TextBox;
+            TextBox address = (TextBox)Satffgrid.Rows[e.RowIndex].FindControl("Address") as TextBox;
+            //updateGrid.Add(name.Text);
+            //updateGrid.Add(address.Text);
+            //Session["Member"] = updateGrid;
+            BindGrid();
+        }
+
+        protected void Satffgrid_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            if (Session["Member"] != null)
+            {
+                List<StaffMember> newGrid = Session["Member"] as List<StaffMember>;
+                newGrid.RemoveAt(e.RowIndex);
+                Session["Member"] = newGrid;
+                BindGrid();
+            }
+
+        }
+
+        protected void Satffgrid_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Attributes.Add("onclick ", String.Format("window.location='Detail_Page.aspx&name'+(Session['Member'] as List<StaffMember>)"));
+            }
+        }
+
+        protected void backCall_Click(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+    }
+}
