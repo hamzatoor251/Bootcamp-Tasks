818e499e4cda7b12321d337704dd7c5b1d48fabf
hamzatoor251 <hamzamushtaqtoor@gmail.com>
2023-05-08 00:07:01 +0500

    Initial commit

---
+++ GridView-Task/Task-1.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Task-1.aspx.cs" Inherits="Gridview_Assign_1.Task_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <asp:gridview id="empgrid" runat="server" backcolor="White" bordercolor="#E7E7FF"
+
+            borderstyle="None" borderwidth="1px" cellpadding="3" font-names="Calibri" font-size="Larger"
+
+            gridlines="Horizontal">
+
+            <AlternatingRowStyle BackColor="#F7F7F7" />
+
+            <FooterStyle BackColor="#B5C7EE" ForeColor="#4A3C88" />
+
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+
+            <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+
+            <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+
+            <SortedAscendingCellStyle BackColor="#F4F4FD" />
+
+            <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+
+            <SortedDescendingCellStyle BackColor="#D8D8F0" />
+
+            <SortedDescendingHeaderStyle BackColor="#3E3277" />
+
+        </asp:gridview>         
+        </div>
+    </form>
+</body>
+</html>
---
+++ GridView-Task/Task-1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gridview_Assign_1
+{
+    public partial class Task_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            empgrid.DataSource = new EmpAdd().MakeEmp();
+            empgrid.DataBind();
+        }
+        public class EmpAdd
+        {
+            public string Name { get; set; }
+            public string Rollno { get; set; }
+            public string Address { get; set; }
+            public List<EmpAdd> MakeEmp()
+            {
+                List<EmpAdd> empList = new List<EmpAdd>();
+                empList.Add(new EmpAdd() { Name = "Ali", Rollno = "01", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Ahmad", Rollno = "02", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Ghufi", Rollno = "03", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Ibi", Rollno = "04", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Haday", Rollno = "05", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Iram", Rollno = "06", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Rida", Rollno = "07", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Tahir", Rollno = "08", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Bilal", Rollno = "09", Address = "Abcd" });
+                empList.Add(new EmpAdd() { Name = "Moadib", Rollno = "10", Address = "Abcd" });
+
+                return empList;
+            }
+
+
+        }
+    }
+}
